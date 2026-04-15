82ad2a9605cf898d5e9f9676a0f6aad59ba6ae9d
ranayoub-1 <Rana.AbuShaar@axsos.academy>
2026-04-15 16:10:16 +0300

    Functions Intermediate II

---
+++ python/python_fundamentals/functions_intermediate_2.py
@@ -0,0 +1,60 @@
+# 1. Update Values in Dictionaries and Lists
+
+x = [ [5,2,3], [10,8,9] ]
+students = [
+    {'first_name': 'Michael', 'last_name': 'Jordan'},
+    {'first_name': 'John', 'last_name': 'Rosales'}
+]
+sports_directory = {
+    'basketball': ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer': ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+
+# updates
+x[1][0] = 15
+students[0]['last_name'] = 'Bryant'
+sports_directory['soccer'][0] = 'Andres'
+z[0]['y'] = 30
+
+
+# 2. Iterate Through a List of Dictionaries
+
+def iterateDictionary(some_list):
+    for item in some_list:
+        output = ""
+        for key, value in item.items():
+            output += f"{key} - {value}, "
+        print(output[:-2])
+
+
+# 3. Get Values From a List of Dictionaries
+
+def iterateDictionary2(key_name, some_list):
+    for item in some_list:
+        print(item[key_name])
+
+
+# 4. Iterate Through a Dictionary with List Values
+
+def printInfo(some_dict):
+    for key in some_dict:
+        print(f"{len(some_dict[key])} {key.upper()}")
+        for value in some_dict[key]:
+            print(value)
+        print()
+
+
+# example data
+students = [
+    {'first_name': 'Michael', 'last_name': 'Jordan'},
+    {'first_name': 'John', 'last_name': 'Rosales'},
+    {'first_name': 'Mark', 'last_name': 'Guillen'},
+    {'first_name': 'KB', 'last_name': 'Tonel'}
+]
+
+dojo = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+
